b10b46a242644df26fd5690ad63bac09a6624024
Denis <94230780+munaashe@users.noreply.github.com>
2023-07-17 14:43:54 +0200

    metadata build fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,23 +3,19 @@ import { SessionProvider } from 'next-auth/react'
 import Footer from './components/Footer'
 import NavBar from './components/NavBar'
 import './globals.css'
-import { Inter } from 'next/font/google'
 import { Provider } from 'react-redux'
 import { store } from './store/store';
-
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata = {
-  title: 'Ernoca',
-  description: 'E-commerce app',
-};
-
+import Head from 'next/head'
 
 export default function RootLayout({
   children, session, ...pageProps }: { children: React.ReactNode, session: any }) {
   
   return (
     <html lang="en">
+      <Head>
+        <title>Ernoca</title>
+        <meta property="og:title" content="E-commerce application" key="title" />
+      </Head>
       <SessionProvider session={session}>
         <Provider store={store}>
           <NavBar />
