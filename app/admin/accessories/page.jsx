dfe55ef37cc58895a8b82284d0456330662a7aac
Denis Siduna <125746473+munaashe@users.noreply.github.com>
2023-10-18 15:37:22 +0200

    show accessories on accessories page

--- app/admin/accessories/page.jsx
+++ app/admin/accessories/page.jsx
@@ -1,9 +1,99 @@
-import React from 'react'
+'use client'
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import SearchBar from '../../components/Searchbar'
+import FilterDropdown from "../../components/FilterDropDown";
+import AdminModalHOC from '../../components/modals/AdminModalHOC';
+import { updateModal } from "../../redux/actions/modals";
+import { useGetAccessoriesQuery } from "../../redux/services/accessoriesService";
+import AccessoryCard from '../../components/adminComponents/AccessoryCard'
+import CarCardLoading from '../../components/adminComponents/CarCardLoading';
+import { sparePartsCategories } from "../../utils/sparePartsCategories";
 
 const Accessories = () => {
+  const [open, setOpen] = useState(false);
+  const dispatch = useDispatch();
+
+  const { data: accessories, isLoading: isGetAccessoriesLoading, refetch: refetchAssessories } = useGetAccessoriesQuery();
+
+  const onAddAccessoryClick = () => {
+    dispatch(updateModal('Add Accessory'));
+    setOpen(true);
+  }
+
+  const skeletonPulses = Array.from({ length: 2 })
+
+  useEffect(() => {
+    refetchAssessories()
+  }, [])
+
   return (
-    <div>Accessories</div>
+    <>
+      <AdminModalHOC
+        open={open}
+        setOpen={setOpen}
+      />
+      <div className="w-full min-h-screen">
+        <div className='p-8 text-[32px] text-center font-semibold'>
+          Accessories
+        </div>
+        <div className='grid grid-cols-3 gap-4 pt-[20px]'>
+          <div className='col-span-2 w-full'>
+            <SearchBar
+              onSearch={() => { }}
+            />
+          </div>
+          <div>
+            <FilterDropdown
+              options={sparePartsCategories}
+              onSelect={() => { }}
+            />
+          </div>
+        </div>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 pt-12 gap-2 items-center justify-center'>
+          <div className="h-300px bg-white shadow-xl rounded-xl flex flex-col items-center justify-center  max-w-[300px] p-8 h-48"
+            onClick={onAddAccessoryClick}
+          >
+            <svg
+              className="w-12 h-12 text-gray-500 cursor-pointer"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+              />
+            </svg>
+            <div className='text-green-500 font-medium text-xl cursor-pointer' >
+              New Accessory
+            </div>
+          </div>
+          {isGetAccessoriesLoading ? (
+            <React.Fragment>
+              {skeletonPulses.map((_, index) => (
+                <CarCardLoading key={index} />
+              ))}
+            </React.Fragment>
+          ) : (
+            <React.Fragment>
+              {accessories?.map((accessory) => (
+                <AccessoryCard
+                  accessory={accessory}
+                  setOpen={setOpen}
+                />
+              ))}
+            </React.Fragment>
+          )}
+        </div>
+      </div>
+    </>
   )
 }
 
-export default Accessories
+export default Accessories;
+
+
--- app/components/adminComponents/AccessoryCard.jsx
+++ app/components/adminComponents/AccessoryCard.jsx
@@ -1,9 +1,73 @@
 import React from 'react'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import Image from 'next/image';
+import { useDispatch } from 'react-redux';
+import { viewCar, updateModal } from '../../redux/actions/modals';
 
-function AccessoryCard() {
+function AccessoryCard({ accessory, setOpen = () => { } }) {
+  const dispatch = useDispatch();
+  const settings = {
+    dots: true,
+    infinite: true,
+    //speed: 5000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  const onView = () => {
+    dispatch(viewCar(accessory));
+    dispatch(updateModal('View Accessory'))
+    setOpen(true);
+  }
   return (
-    <div>AccessoryCard</div>
+    <div>
+      <div className='flex flex-wrap justify-center items-center'>
+        <div className='container mx-auto'>
+          <div className='relative flex flex-col min-w-0 break-words bg-white mb-6 shadow-xl rounded-lg'>
+            <div className='px-6 flex flex-col'>
+            </div>
+            <div className='text-center text-[#7f2d00] font-semibold text-[18px]'>
+              {accessory.name}
+            </div>
+            <div className='pb-8'>
+              <Slider {...settings}>
+                {accessory.images.map((image, index) => (
+                  <div key={index}>
+                    <Image
+                      src={image}
+                      alt=''
+                      height={200}
+                      width={300}
+                      className='h-[220px] rounded-xl'
+                    />
+                  </div>
+                ))}
+              </Slider>
+            </div>
+            <div className='text-center text-[#7f2d00] text-[14px]'>
+              $ {accessory.price}
+            </div>
+            <div className='px-2 text-center py-4'>
+              Seller: +{accessory.phone}
+            </div>
+            <div className='flex justify-center items-center pb-4'>
+              <div
+                className='rounded-xl bg-[#201c78] hover:bg-[#464686] text-white text-center px-4 py-1 w-1/2 cursor-pointer'
+                onClick={onView}
+              >
+                Details
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
 export default AccessoryCard
+
+
+
+
