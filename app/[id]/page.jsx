0b4344011e189ae84183be7ba017505cff0aad55
munaashe <denis.siduna@gmail.com>
2023-11-24 11:43:22 +0200

    generate static params for items page

---
+++ app/[id]/page.jsx
@@ -0,0 +1,28 @@
+
+import React from 'react'
+import { db } from '../../firebase'
+import { collection, getDocs } from 'firebase/firestore';
+
+export async function generateStaticParams() {
+    const carsRef = collection(db, 'cars');
+    const accessoriesRef = collection(db, 'accessories');
+
+    const carsSnapshot = await getDocs(carsRef);
+    const accessoriesSnapshot = await getDocs(accessoriesRef);
+    const cars = carsSnapshot.docs.map((doc) => ({ id: doc.id }));
+    const accessories = accessoriesSnapshot.docs.map((doc) => ({ id: doc.id}));
+
+    const Arr = [...cars, ...accessories]
+
+    return Arr.map((item) => ({
+        id: item.id,
+    }))
+}
+
+const Product = () => {
+    return (
+        <div>Product</div>
+    )
+}
+
+export default Product
