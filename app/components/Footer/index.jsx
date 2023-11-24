c7e176638648c94e6664a9cd5ef12bc90be6bbb1
munaashe <denis.siduna@gmail.com>
2023-11-24 08:44:19 +0200

    update footer links

--- app/components/Footer/index.jsx
+++ app/components/Footer/index.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import logo from '../../assets/logo.png'
+import Link from "next/link";
 
 export default function Footer() {
   return (
@@ -129,16 +130,11 @@ export default function Footer() {
               POLICY
             </h6>
 
-            {[
-              "Privacy Policy",
-              "Terms & Condition",
-              "Return Policy",
-              "Cancelation Policy",
-            ].map((e) => (
-              <p className="mb-4 text-xl sm:text-base" key={e}>
-                <a href="#!" className="text-gray-600">
-                  {e ?? "Footer Link"}
-                </a>
+            {footerLinks.map((link) => (
+              <p className="mb-4 text-xl sm:text-base" key={link.name}>
+                <Link href={link.link} target='_blank' className="text-gray-600">
+                  {link.name ?? "Footer Link"}
+                </Link>
               </p>
             ))}
           </div>
@@ -199,7 +195,7 @@ export default function Footer() {
                   d="M493.4 24.6l-104-24c-11.3-2.6-22.9 3.3-27.5 13.9l-48 112c-4.2 9.8-1.4 21.3 6.9 28l60.6 49.6c-36 76.7-98.9 140.5-177.2 177.2l-49.6-60.6c-6.8-8.3-18.2-11.1-28-6.9l-112 48C3.9 366.5-2 378.1.6 389.4l24 104C27.1 504.2 36.7 512 48 512c256.1 0 464-207.5 464-464 0-11.2-7.7-20.9-18.6-23.4z"
                 ></path>
               </svg>
-              +263 12 345 6789
+              +263 78 599 2643
             </p>
           </div>
         </div>
@@ -213,3 +209,18 @@ export default function Footer() {
     </footer>
   );
 }
+
+const footerLinks = [
+  {
+    name: 'Privacy Policy',
+    link: '/privacy'
+  },
+  {
+    name: 'Terms & Conditions',
+    link: '/terms'
+  },
+  {
+    name: 'Cancelation Policy',
+    link: '/cancelation'
+  },
+]
