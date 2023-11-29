3234354c0a3e3f3f7cbb62a167b4a7d36528ae15
munaashe <denis.siduna@gmail.com>
2023-11-29 17:14:02 +0200

    add redux persist

--- app/components/Providers.jsx
+++ app/components/Providers.jsx
@@ -4,17 +4,20 @@ import { Provider } from "react-redux"
 import configureAppStore from "../redux/store/configurestore"
 import LayoutProvider from "./LayoutProvider"
 import { AuthContextProvider } from "./context/AuthContext"
+import { PersistGate } from 'redux-persist/integration/react'
 
 const { store, persistor } = configureAppStore();
 
 const Providers = ({ children }) => {
     return (
         <Provider store={store}>
-            <AuthContextProvider>
-                <LayoutProvider>
-                    {children}
-                </LayoutProvider>
-            </AuthContextProvider>
+            <PersistGate loading={null} persistor={persistor}>
+                <AuthContextProvider>
+                    <LayoutProvider>
+                        {children}
+                    </LayoutProvider>
+                </AuthContextProvider>
+            </PersistGate>
         </Provider>
     )
 }
