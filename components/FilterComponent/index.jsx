6af1f52a479e7e48c342ba320579f427d2241b54
munaashe <denis.siduna@gmail.com>
2024-01-24 12:02:36 +0200

    filter component fix

--- components/FilterComponent/index.jsx
+++ components/FilterComponent/index.jsx
@@ -15,7 +15,6 @@ const initialState = {
     transmission: [],
     fuel: [],
     location: null,
-    minMileage: 0,
     maxMileage: 300000,
 };
 
@@ -50,17 +49,10 @@ const FilterComponent = ({
                 return false;
             }
 
-            // Filter by min mileage
-            if (
-                filters.minMileage &&
-                parseInt(car.mileage) < parseInt(filters.minMileage)
-            ) {
-                return false;
-            }
-
             // Filter by max mileage
             if (
-                filters.maxMileage &&
+                filters.maxMileage !== null &&
+                filters.maxMileage !== 0 &&
                 parseInt(car.mileage) > parseInt(filters.maxMileage)
             ) {
                 return false;
@@ -128,109 +120,117 @@ const FilterComponent = ({
         }))
     };
 
+    const setMaxMileage = (mileage) => {
+        setFilters((prevFilters) => ({
+            ...prevFilters,
+            maxMileage: parseInt(mileage.target.value)
+        }))
+    };
+
     const setSelectedItems = (selectedItemsUpdated, filterName) => {
         setFilters((prevState) => ({
             ...prevState,
             [filterName]: selectedItemsUpdated,
         }));
     };
-    console.log(filters.maxPrice)
     return (
-        <div className="bg-gray-100 p-4 m-8 mt-2 text-slate-900 rounded-xl shadow-md">
-            <div className="font-semibold text-[20px] pb-4">Search for your dream car</div>
-            <div className="grid grid-cols-4 gap-2 xs:grid-cols-1">
-                <div className='flex flex-col gap-2'>
-                    <MultipleSelect
-                        label="Makes"
-                        filterName="make"
-                        items={makes}
-                        selectedItems={filters.make}
-                        setSelectedItems={setSelectedItems}
-                    />
-                    <MultipleSelect
-                        label="Condition"
-                        filterName="condition"
-                        items={conditions}
-                        selectedItems={filters.condition}
-                        setSelectedItems={setSelectedItems}
-                    />
-                </div>
-                <div className='flex flex-col gap-2'>
-                    <MultipleSelect
-                        label="Transmission"
-                        filterName="transmission"
-                        items={transmission}
-                        selectedItems={filters.transmission}
-                        setSelectedItems={setSelectedItems}
-                    />
-                    <MultipleSelect
-                        label="Fuel"
-                        filterName="fuel"
-                        items={fuel}
-                        selectedItems={filters.fuel}
-                        setSelectedItems={setSelectedItems}
-                    />
-                </div>
-                <div className='flex flex-col gap-2'>
-                    <div className="w-full grid grid-cols-2 px-4 items-center">
-                        <label>Budget:</label>
-                        <input
-                            type="text"
-                            name="maxPrice"
-                            className="w-full mr-8 rounded-md py-1 pl-2"
-                            value={filters.maxPrice}
-                            onChange={handleChange}
+        <div className='my-4 px-8 xs:px-4 sm:px-4 xs:mx-auto w-full'>
+            <div className="bg-gray-100 p-4 text-slate-900 rounded-xl shadow-md w-full flex flex-col items-center justify-center">
+                <div className="font-semibold text-[24px] sm:text-[16px] md:text-[20px] xs:text-[14px] text-center pb-4">Search for your dream car</div>
+                <div className="grid grid-cols-4 gap-2 xs:grid-cols-1 sm:grid-cols-1 md:grid-cols-2 w-full">
+                    <div className='flex flex-col gap-2 w-full'>
+                        <MultipleSelect
+                            label="Makes"
+                            filterName="make"
+                            items={makes}
+                            selectedItems={filters.make}
+                            setSelectedItems={setSelectedItems}
+                        />
+                        <MultipleSelect
+                            label="Condition"
+                            filterName="condition"
+                            items={conditions}
+                            selectedItems={filters.condition}
+                            setSelectedItems={setSelectedItems}
                         />
                     </div>
-                    <div className=" grid grid-cols-2 px-4 items-center">
-                        <label>Location:</label>
-                        <CitySuggestInput
-                            location={filters.location}
-                            towns={towns}
-                            setLocation={setLocation}
+                    <div className='flex flex-col gap-2'>
+                        <MultipleSelect
+                            label="Transmission"
+                            filterName="transmission"
+                            items={transmission}
+                            selectedItems={filters.transmission}
+                            setSelectedItems={setSelectedItems}
+                        />
+                        <MultipleSelect
+                            label="Fuel"
+                            filterName="fuel"
+                            items={fuel}
+                            selectedItems={filters.fuel}
+                            setSelectedItems={setSelectedItems}
                         />
                     </div>
-                </div>
+                    <div className='flex flex-col gap-2'>
+                        <div className="w-full grid grid-cols-2 px-4 items-center">
+                            <label>Budget:</label>
+                            <input
+                                type="text"
+                                name="maxPrice"
+                                className="w-full mr-8 rounded-md py-1 pl-2"
+                                value={filters.maxPrice}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className=" grid grid-cols-2 px-4 items-center">
+                            <label>Location:</label>
+                            <CitySuggestInput
+                                location={filters.location}
+                                towns={towns}
+                                setLocation={setLocation}
+                            />
+                        </div>
+                    </div>
 
-                <div className='flex flex-col gap-2'>
-                    <div className="w-full px-4 flex gap-4 justify-between items-center">
-                        <label>Mnf:</label>
-                        <YearPicker
-                            name="minYear"
-                            years={minYears.sort((a, b) => a - b)}
-                            selectedYear={filters.minYear}
-                            onChange={handleChange}
-                        />
-                        <div>to</div>
-                        <YearPicker
-                            name="maxYear"
-                            years={maximumYears.sort((a, b) => b - a)}
-                            selectedYear={filters.maxYear}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div className="w-full flex px-4 gap-4 justify-between items-center">
-                        <label>Mileage:</label>
-                        <MileageRangeComponent
-                            initialMaxMileage={filters.maxMileage}
-                            initialMinMileage={filters.minMileage}
-                        />
+                    <div className='flex flex-col gap-2'>
+                        <div className="w-full px-4 flex gap-4 justify-between items-center">
+                            <label>Mnf:</label>
+                            <YearPicker
+                                name="minYear"
+                                years={minYears.sort((a, b) => a - b)}
+                                selectedYear={filters.minYear}
+                                onChange={handleChange}
+                            />
+                            <div>to</div>
+                            <YearPicker
+                                name="maxYear"
+                                years={maximumYears.sort((a, b) => b - a)}
+                                selectedYear={filters.maxYear}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="w-full flex px-4 gap-4 justify-between items-center">
+                            <label>Max Mileage:</label>
+                            <MileageRangeComponent
+                                maxMileage={filters.maxMileage}
+                                setMaxMileage={setMaxMileage}
+                            />
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="flex justify-center items-center gap-8 pt-4">
-                <button
-                    className="rounded-lg bg-[#32348e] hover:bg-[#bb2433] text-white px-4 py-1"
-                    onClick={handleFilter}
-                >
-                    Filter
-                </button>
-                <button
-                    className="rounded-lg bg-[#32348e] hover:bg-[#bb2433] text-white px-4 py-1"
-                    onClick={resetFilter}
-                >
-                    Reset
-                </button>
+                <div className="flex justify-center items-center gap-8 pt-4">
+                    <button
+                        className="rounded-lg bg-[#32348e] hover:bg-[#bb2433] text-white px-4 py-1"
+                        onClick={handleFilter}
+                    >
+                        Filter
+                    </button>
+                    <button
+                        className="rounded-lg bg-[#32348e] hover:bg-[#bb2433] text-white px-4 py-1"
+                        onClick={resetFilter}
+                    >
+                        Reset
+                    </button>
+                </div>
             </div>
         </div>
     );
@@ -242,7 +242,7 @@ const YearPicker = ({ selectedYear, onChange, years, name }) => {
     return (
         <select
             id="year"
-            className="border border-gray-300 rounded px-2 py-1 mt-1"
+            className="border border-gray-300 rounded px-2 py-1 mt-1 xs:text-sm sm:text-sm xs:leading-2 sm:text-sm"
             value={selectedYear}
             onChange={onChange}
             name={name}
@@ -303,7 +303,7 @@ export const CitySuggestInput = ({ location, towns, setLocation = () => { } }) =
                 value={value}
                 onChange={handleChange}
                 placeholder="Car Location"
-                className='w-full mr-8 rounded-md py-1 pl-2 border border-gray-300'
+                className='w-full mr-8 rounded-md py-1 pl-2 border border-gray-300 xs:text-sm xs:leading-2'
             />
             {isOpen && (
                 <ul className="absolute z-10 bg-white mt-2 py-1 rounded-md shadow-lg">
@@ -311,7 +311,7 @@ export const CitySuggestInput = ({ location, towns, setLocation = () => { } }) =
                         <li
                             key={index}
                             onClick={() => handleSuggestionClick(suggestion)}
-                            className="px-4 py-2 cursor-pointer hover:bg-gray-100"
+                            className="px-4 py-2 cursor-pointer hover:bg-gray-100 xs:text-sm xs:leading-2"
                         >
                             {suggestion}
                         </li>
@@ -322,25 +322,8 @@ export const CitySuggestInput = ({ location, towns, setLocation = () => { } }) =
     );
 };
 
-const MileageRangeComponent = ({ initialMinMileage, initialMaxMileage, onRangeChange }) => {
-    const [minMileage, setMinMileage] = useState(initialMinMileage);
-    const [maxMileage, setMaxMileage] = useState(initialMaxMileage);
+const MileageRangeComponent = ({ maxMileage, setMaxMileage = () => { } }) => {
 
-    useEffect(() => {
-        if (minMileage >= maxMileage) {
-            setMaxMileage(minMileage + 50000);
-        }
-    }, [minMileage]);
-
-    const handleMinMileageChange = (event) => {
-        const newMinMileage = parseInt(event.target.value);
-        setMinMileage(newMinMileage);
-    };
-
-    const handleMaxMileageChange = (event) => {
-        const newMaxMileage = parseInt(event.target.value);
-        setMaxMileage(newMaxMileage);
-    };
 
     const renderMileageOptions = () => {
         const options = [];
@@ -353,27 +336,18 @@ const MileageRangeComponent = ({ initialMinMileage, initialMaxMileage, onRangeCh
         }
         options.push(
             <option key={500000} value={500000}>
-                400,000+
+                400k +
             </option>
         );
         return options;
     };
 
     return (
-        <div className='flex gap-1 justify-between items-center'>
-            <select
-                id="minMileage"
-                value={minMileage}
-                onChange={handleMinMileageChange}
-                className='py-1 rounded-md'
-            >
-                {renderMileageOptions()}
-            </select>
-            <div>to</div>
+        <div className='flex gap-1 justify-between items-center xs:text-sm xs:leading-2 sm:text-sm sm:leading-4'>
             <select
                 id="maxMileage"
                 value={maxMileage}
-                onChange={handleMaxMileageChange}
+                onChange={setMaxMileage}
                 className='py-1 rounded-md'
             >
                 {renderMileageOptions()}
@@ -436,12 +410,12 @@ const MultipleSelect = ({ label, filterName, items, selectedItems, setSelectedIt
                     open={isOpen}
                 >
                     {() => (
-                        < div className='grid grid-cols-2 px-4 w-full items-center'>
+                        < div className='grid grid-cols-2 px-4 w-full items-center xs:text-md'>
                             <label>{label}:</label>
                             <div className="relative">
                                 <span className="inline-block w-full rounded-md shadow-sm z-10">
                                     <Listbox.Button
-                                        className="cursor-default relative w-full rounded-md border border-gray-300 bg-white px-2 py-2 text-left focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition ease-in-out duration-150 sm:text-sm sm:leading-5"
+                                        className="cursor-default relative w-full rounded-md border border-gray-300 bg-white px-2 py-2 text-left focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition ease-in-out duration-150 sm:text-sm xs:text-sm xs:leading-2 sm:leading-5"
                                         onClick={() => setIsOpen(!isOpen)}
                                         open={isOpen}
                                     >
@@ -489,7 +463,7 @@ const MultipleSelect = ({ label, filterName, items, selectedItems, setSelectedIt
                                                             className={`${active
                                                                 ? "text-white bg-[#32348e]"
                                                                 : "text-gray-900"
-                                                                } cursor-default select-none relative py-2 pl-8 pr-4`}
+                                                                } cursor-default select-none relative py-2 pl-8 pr-4 xs:text-sm xs:leading-2 sm:text-sm xs:leading-4`}
                                                         >
                                                             <span
                                                                 className={`${selected ? "font-semibold" : "font-normal"
