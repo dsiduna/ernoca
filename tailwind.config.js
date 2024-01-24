ccfd7a66cce375ee1b5d1f53c4475a9da09f3091
munaashe <denis.siduna@gmail.com>
2024-01-24 12:02:10 +0200

    fix small screens breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,8 @@ module.exports = {
     extend: {
       screens: {
         'xs': { 'max': '390px' },
+        'sm': { 'min': '391px', 'max': '760px' },
+        'md': { 'min': '761px', 'max': '1024px' },
       },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
